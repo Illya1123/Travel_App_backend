24a59113719eb779319ab3789cd39ac8d47956f4
Illya1123 <21520565@gm.uit.edu.vn>
2025-07-21 19:18:12 +0700

    cập nhật lại api thay đổi setting thanh toán, cần quyền cao nhất của Admin

--- src/routes/paymentmethod.js
+++ src/routes/paymentmethod.js
@@ -5,15 +5,17 @@ import {
     updatePaymentMethodStatus,
 } from '../controllers/paymentMethodController.js'
 
+import { verifyAccessToken, isAdmin } from '../middlewares/verifyToken.js'
+
 const router = express.Router()
 
 // Lấy tất cả phương thức thanh toán
 router.get('/', getPaymentMethods)
 
 // Khởi tạo phương thức thanh toán mặc định
-router.post('/seed', seedPaymentMethods)
+router.post('/seed', [verifyAccessToken, isAdmin], seedPaymentMethods)
 
 // Cập nhật trạng thái theo name
-router.put('/:name', updatePaymentMethodStatus)
+router.put('/:name', [verifyAccessToken, isAdmin], updatePaymentMethodStatus)
 
 export default router
